e49a08264fc7f46888c9ef17ab3cb1d479268945
Brian <josephbrianazarraga@gmail.com>
2026-05-12 22:21:58 +0800

    Fixed sizing issue for InfoCard

--- src/projectsPage/Projects/Phonix.tsx
+++ src/projectsPage/Projects/Phonix.tsx
@@ -34,7 +34,16 @@ export default function phonixPage() {
           I primarily worked on frontend code, UI design, art design, branding, and refactoring and cleaning up old AI generated code.
 	  </p>
 	</div>
+      </div>
 
+      <div className = "phonixFooter">
+        <p className = "phonixStackText"> 
+          <span style = {{color: "rgba(80,185,255,1)"}}> [TypeScript]</span>
+	  <span style = {{color: "rgba(65,185,255,1)"}}> [React]</span>
+	  <span style = {{color: "rgba(200,125,255,1)"}}> [CSS]</span>
+	  <span style = {{color: "rgba(255,200,50,1)"}}> [Cloudflare D1]</span>
+	  <span style = {{color: "rgba(0,150,255,1"}}> [Gemini API]</span>
+	</p>
       </div>
     </div>
 
